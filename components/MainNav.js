fac7f5301e80af3720344ae2834dd03192e9fd0f
unknown <ouyangphoenix@gmail.com>
2026-02-03 22:35:17 -0500

    add navbar code

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,5 +1,9 @@
+import Link from "next/link";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
 export default function MainNav() {
     return(<>
         <p>MainNav</p>
+        
     </>)
 }
